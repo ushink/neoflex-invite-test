3ffd33289adf113493340c4a3e25db941df29740
Ulyana <ushinkevich@mail.ru>
2024-03-28 21:34:37 +0400

    products  list components

--- src/page/Main/main.jsx
+++ src/page/Main/main.jsx
@@ -1,44 +1,18 @@
+import ProductsList from '../../components/Products/productsList'
 import { wiredHeadset, wirelessHeadset } from '../../mock/poducts'
 import s from './main.module.css'
 
 export default function Main() {
+    console.log(wiredHeadset)
     return (
         <main className={s.main}>
             <section className={s.section}>
                 <h2 className={s.title}>Наушники</h2>
-                <div className={s.products}>
-                    {wiredHeadset.map((el) => (
-                        <div className={s.productCard} key={Math.random()}>
-                            <img className={s.image} src={el.img} alt="" />
-                            <div className={s.info}>
-                                <h3 className={s.name}>{el.title}</h3>
-                                <p className={s.price}>{el.price}</p>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.rate}>{el.rate}</span>
-                                <button className={s.buttonBuy}>Купить</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <ProductsList products={wiredHeadset} />
             </section>
             <section className={s.section}>
                 <h2 className={s.title}>Беспроводные наушники</h2>
-                <div className={s.products}>
-                    {wirelessHeadset.map((el) => (
-                        <div className={s.productCard} key={Math.random()}>
-                            <img className={s.image} src={el.img} alt="" />
-                            <div className={s.info}>
-                                <h3 className={s.name}>{el.title}</h3>
-                                <p className={s.price}>{el.price}</p>
-                            </div>
-                            <div className={s.info}>
-                                <span className={s.rate}>{el.rate}</span>
-                                <button className={s.buttonBuy}>Купить</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <ProductsList products={wirelessHeadset} />
             </section>
         </main>
     )
