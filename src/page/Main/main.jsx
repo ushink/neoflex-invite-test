cd4c1b4cee2bfa02f69322e2bcfbe65b198f32d4
Ulyana <ushinkevich@mail.ru>
2024-03-29 10:04:25 +0400

    layout header

--- src/page/Main/main.jsx
+++ src/page/Main/main.jsx
@@ -1,3 +1,4 @@
+import Header from '../../components/Header/header'
 import ProductsList from '../../components/Products/productsList'
 import { wiredHeadset, wirelessHeadset } from '../../mock/poducts'
 import s from './main.module.css'
@@ -5,15 +6,18 @@ import s from './main.module.css'
 export default function Main() {
     console.log(wiredHeadset)
     return (
-        <main className={s.main}>
-            <section className={s.section}>
-                <h2 className={s.title}>Наушники</h2>
-                <ProductsList products={wiredHeadset} />
-            </section>
-            <section className={s.section}>
-                <h2 className={s.title}>Беспроводные наушники</h2>
-                <ProductsList products={wirelessHeadset} />
-            </section>
-        </main>
+        <>
+            <Header />
+            <main className={s.main}>
+                <section className={s.section}>
+                    <h2 className={s.title}>Наушники</h2>
+                    <ProductsList products={wiredHeadset} />
+                </section>
+                <section className={s.section}>
+                    <h2 className={s.title}>Беспроводные наушники</h2>
+                    <ProductsList products={wirelessHeadset} />
+                </section>
+            </main>
+        </>
     )
 }
