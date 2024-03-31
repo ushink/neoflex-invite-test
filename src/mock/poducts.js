d09daad167d2a01858ff181871a6bcdf1a998e93
Ulyana <ushinkevich@mail.ru>
2024-03-31 10:31:17 +0400

    new random id for mock data

--- src/mock/poducts.js
+++ src/mock/poducts.js
@@ -1,50 +1,44 @@
 export const wiredHeadset = [
     {
-        id: Math.random(),
+        id: 'e9803f7b40204d6796ae15ecb0910ea8',
         img: './img/assets/S8521.png',
         title: 'Apple BYZ S852I',
-        count: 1,
         price: 2927,
         oldPrice: 3527,
         rate: 4.7
     },
     {
-        id: Math.random(),
+        id: '62cb73505a4f458899ad9aad4970bd8c',
         img: './img/assets/EarPods1.png',
         title: 'Apple EarPods',
-        count: 1,
         price: 2327,
         rate: 4.5
     },
     {
-        id: Math.random(),
+        id: '151d855e66ca442488cda324658a0203',
         img: './img/assets/EarPods2.png',
         title: 'Apple EarPods',
-        count: 1,
         price: 2327,
         rate: 4.5
     },
     {
-        id: Math.random(),
+        id: 'fb76e58304684cf5aca453a9c5820092',
         img: './img/assets/S8521.png',
         title: 'Apple BYZ S852I',
-        count: 1,
         price: 2927,
         rate: 4.7
     },
     {
-        id: Math.random(),
+        id: '008845c117944e94a3017f37c1aef880',
         img: './img/assets/EarPods1.png',
         title: 'Apple EarPods',
-        count: 1,
         price: 2327,
         rate: 4.5
     },
     {
-        id: Math.random(),
+        id: '5fa9d242c6f94a72a0e6a7d1d88b9758',
         img: './img/assets/EarPods2.png',
         title: 'Apple EarPods',
-        count: 1,
         price: 2327,
         rate: 4.5
     }
@@ -52,26 +46,23 @@ export const wiredHeadset = [
 
 export const wirelessHeadset = [
     {
-        id: Math.random(),
+        id: '12503b0ca723430e870ffec9e33d2d8e',
         img: './img/assets/AirPods.png',
         title: 'Apple AirPods',
-        count: 1,
         price: 9527,
         rate: 4.7
     },
     {
-        id: Math.random(),
+        id: 'c8727e675974420393279b58a196b29e',
         img: './img/assets/GH04.png',
         title: 'GERLAX GH-04',
-        count: 1,
         price: 6527,
         rate: 4.7
     },
     {
-        id: Math.random(),
+        id: 'a8750ca13dbc4e95b728e1a794a9a3d9',
         img: './img/assets/BO4.png',
         title: 'BOROFONE BO4',
-        count: 1,
         price: 7527,
         rate: 4.7
     }
