250361e430dd8a19ddd7f838e25d24fe073fad60
Ulyana <ushinkevich@mail.ru>
2024-03-31 10:42:11 +0400

    delete button

--- src/components/Products/productsList.jsx
+++ src/components/Products/productsList.jsx
@@ -25,7 +25,9 @@ export default function ProductsList({ products }) {
                     <img className={s.image} src={el.img} alt="" />
                     <div className={s.info}>
                         <h3 className={s.name}>{el.title}</h3>
-                        <p className={s.price}>{el.price}</p>
+                        <p className={s.price}>
+                            {el.price.toLocaleString('ru-RU')}
+                        </p>
                     </div>
                     <div className={s.info}>
                         <span className={s.rate}>{el.rate}</span>
--- src/page/Cart/cart.jsx
+++ src/page/Cart/cart.jsx
@@ -6,12 +6,16 @@ import Counter from '../../components/Counter/counter'
 import { useMyContext } from '../../context/myContext'
 
 export default function Cart() {
-    const { cart } = useMyContext()
+    const { cart, setCart } = useMyContext()
 
     function getTotalPrice() {
         return cart.reduce((totalPrice, item) => {
             return totalPrice + item.price * item.quantity
         }, 0)
+    }
+
+    function deleteProduct(id) {
+        setCart(cart.filter((item) => item.id !== id))
     }
 
     return (
@@ -23,7 +27,10 @@ export default function Cart() {
                     <section className={s.products}>
                         {cart?.map((el) => (
                             <div className={s.product} key={el.id}>
-                                <button className={s.delete}>
+                                <button
+                                    className={s.delete}
+                                    onClick={() => deleteProduct(el.id)}
+                                >
                                     <MainIconsSvg id={'delete'} />
                                 </button>
 
@@ -31,14 +38,16 @@ export default function Cart() {
                                     <img
                                         className={s.img}
                                         src={el.src}
-                                        alt=""
+                                        alt={el.name}
                                     />
                                     <Counter counts={el.quantity} id={el.id} />
                                 </div>
 
                                 <div className={s.info}>
                                     <p className={s.name}>{el.name}</p>
-                                    <p className={s.price}>{el.price}</p>
+                                    <p className={s.price}>
+                                        {el.price.toLocaleString('ru-RU')}
+                                    </p>
                                 </div>
 
                                 <span className={s.allPrice}>
